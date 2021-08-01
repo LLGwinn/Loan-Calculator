11cf004308cd1022f63c4e8e4b917cadcd75940a
Laura Lee Gwinn <ells.gwinn@gmail.com>
2021-08-01 14:49:14 -0400

    added tests and js function content

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,55 @@
+describe('input error tests', function() {
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
+    it('should handle <=0 values correctly', function () {
+        // How do you test for alerts?
+    });
 
+    it('should handle non-number entries correctly', function() {
+        // How do you test for alerts?
+    });
+})
+
+describe('calculateMonthlyPmt() tests', function() {
+
+    it('should calculate the monthly rate correctly', function () {
+        expect(
+            calculateMonthlyPayment( 
+                { amount: 10000,
+                  years: 5,
+                  rate: 7.5 }
+            )).toEqual('$200.38');
+    });
+
+    it('should handle zero interest rate correctly', function () {
+        expect(
+            calculateMonthlyPayment( 
+                { amount: 10000,
+                 years: 5,
+                 rate: 0 }
+            )).toEqual('$166.67');
+    });
+
+})
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+    expect(
+        calculateMonthlyPayment( 
+            { amount: 12000,
+              years: 10,
+              rate: 0 }
+        )).toEqual('$100.00');
+
+    expect(
+         calculateMonthlyPayment( 
+            { amount: 12000,
+              years: 10,
+              rate: 5.7575 }
+        )).toEqual('$131.77');
 });
 
-/// etc
+
+
+
+
+
+
--- calculator.js
+++ calculator.js
@@ -21,21 +21,85 @@ window.addEventListener('DOMContentLoaded', function() {
   // Put some default values in the inputs
   // Call a function to calculate the current monthly payment
   function setupIntialValues() {
+    let p = document.getElementById("loan-amount");
+    let t = document.getElementById("loan-years");
+    let i = document.getElementById("loan-rate");
+    let pmt = document.getElementById("monthly-payment");
+    p.value = 10000;
+    t.value = 5;
+    i.value = 7.5;
+    pmt.value = calculateMonthlyPayment(
+        {amount: p.value, 
+         years: t.value, 
+         rate: i.value
+        });  
   }
   
   // Get the current values from the UI
   // Update the monthly payment
   function update() {
+    jasmine.createSpy();
+    myAlert = spyOn(window, 'alert');
+    let numericMsg = "All values should be numeric."
+    let zeroMsg = ("Values should be greater than zero.")
+    let data = getCurrentUIValues();
+    if (isNaN(data.amount)) {
+        alert(numericMsg);
+        updateMonthly("");
+        return;
+    }
+    if (data.amount < 0) {
+        alert(zeroMsg);
+        updateMonthly("");
+        return;
+    }
+    if (isNaN(data.years)) {
+        alert(numericMsg);
+        updateMonthly("");
+        return;
+    }
+    if (data.years <= 0) {
+        alert(zeroMsg);
+        updateMonthly("");
+        return;
+    }
+    if (isNaN(data.rate)) {
+        alert(numericMsg);
+        updateMonthly("");
+        return;
+    }
+    if (data.rate < 0) {
+        alert(zeroMsg);
+        updateMonthly("");
+        return;
+    }
+    let pmt = document.getElementById("monthly-payment");
+    pmt.value = calculateMonthlyPayment(
+        {amount: data.amount, 
+         years: data.years, 
+         rate: data.rate
+        });
+    updateMonthly(pmt.value);
   }
   
   // Given an object of values (a value has amount, years and rate ),
   // calculate the monthly payment.  The output should be a string
   // that always has 2 decimal places.
   function calculateMonthlyPayment(values) {
+      let amt = values.amount;
+      let time = values.years * 12;
+      let rate = values.rate/12/100;
+      let mnthly = (amt * rate) / (1 - Math.pow(1 + rate, 0 - time) );
+      if (rate === 0) {
+          mnthly = amt/time;
+      }
+      return `$${mnthly.toFixed(2)}`; 
   }
   
   // Given a string representing the monthly payment value,
   // update the UI to show the value.
   function updateMonthly(monthly) {
+    let mnthlyPmt = document.getElementById("monthly-payment");
+    mnthlyPmt.innerText = monthly;
   }
   
